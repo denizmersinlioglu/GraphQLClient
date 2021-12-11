2543537adda2779789f1a6d0751f09253f53df6e
Deniz <mersinliogludeniz@gmail.com>
2021-12-11 13:20:24 +0300

    UserDefaultsLocalDatabase + Database Notifier Implemented

--- GraphQLClient.playground/Contents.swift
+++ GraphQLClient.playground/Contents.swift
@@ -5,230 +5,234 @@ import SwiftyJSON
 // MARK: - ApiClient
 
 struct ApiClient {
-    
-    // MARK: Properties
-    
-    private let baseURL: () -> String
-    private let accessToken: () -> String?
-    private let logger = Logger()
-    
-    // MARK: Life Cycle
 
-    init(
-        baseURL: @escaping () -> String,
-        accessToken: @escaping () -> String?
-    ) {
-        self.baseURL = baseURL
-        self.accessToken = accessToken
-    }
-    
-    // MARK: Methods
-    
-    func perform<T: JSONParsable>(_ operation: GraphQL.Operation, as: T.Type) -> AnyPublisher<T, GraphQL.Error> {
-        URLSession.shared.dataTaskPublisher(for: httpRequest(for: operation))
-            .handleEvents(
-                receiveSubscription: { _ in logRequest(for: operation) },
-                receiveOutput: { logResponse(for: operation, output: $0) }
-            )
-            .tryMap { try JSON(data: $0.data) }
-            .mapError { GraphQL.Error($0) }
-            .flatMap { json -> AnyPublisher<T, GraphQL.Error> in
-                let data = json["data"]
-                let errors = json["errors"]
-                if data.exists(),
-                   let result = T.init(data[operation.parsingKey]) {
-                    return Just(result)
-                        .setFailureType(to: GraphQL.Error.self)
-                        .eraseToAnyPublisher()
-                } else if errors.exists(),
-                          let error = errors.arrayValue.compactMap(GraphQL.Error.init).first {
-                    return Fail(error: error)
-                        .eraseToAnyPublisher()
-                } else {
-                    return Fail(error: GraphQL.Error.somethingWentWrong)
-                        .eraseToAnyPublisher()
-                }
-            }
-            .eraseToAnyPublisher()
-    }
-    
-    // MARK: Helpers
-    
-    private func httpRequest(for operation: GraphQL.Operation) -> URLRequest {
-        var urlRequest = URLRequest(url: URL(string: baseURL())!)
-        urlRequest.httpMethod = "POST"
-        urlRequest.httpBody = try? operation.request.json.rawData()
-        urlRequest.addValue("application/json", forHTTPHeaderField: "content-type")
-        if let token = accessToken() {
-            urlRequest.addValue(token, forHTTPHeaderField: "Authorization")
-        }
-        return urlRequest
-    }
-    
-    private func logRequest(for operation: GraphQL.Operation) {
-        logger.log(
-            "GraphQL Request",
-            [
-                "Operation: \(operation)",
-                "Parameters: \(operation.request.variables)",
-            ]
-        )
-    }
-    
-    private func logResponse(
-        for operation: GraphQL.Operation,
-        output: URLSession.DataTaskPublisher.Output
-    ) {
-        logger.log(
-            "GraphQL Response",
-            [
-                "Status: \((output.response as? HTTPURLResponse)?.statusCode ?? 0)",
-                "Operation: \(operation)",
-                "Parameters: \(operation.request.variables)",
-                "Data: \((try? JSON(data: output.data)) ?? [:])"
-            ]
-        )
-    }
+	// MARK: Properties
+
+	private let baseURL: () -> String
+	private let accessToken: () -> String?
+	private let logger = Logger()
+
+	// MARK: Life Cycle
+
+	init(
+		baseURL: @escaping () -> String,
+		accessToken: @escaping () -> String?
+	) {
+		self.baseURL = baseURL
+		self.accessToken = accessToken
+	}
+
+	// MARK: Methods
+
+	func perform<T: JSONParsable>(_ operation: GraphQL.Operation, as: T.Type) -> AnyPublisher<T, GraphQL.Error> {
+		URLSession.shared.dataTaskPublisher(for: httpRequest(for: operation))
+			.handleEvents(
+				receiveSubscription: { _ in logRequest(for: operation) },
+				receiveOutput: { logResponse(for: operation, output: $0) }
+			)
+			.tryMap { try JSON(data: $0.data) }
+			.mapError { GraphQL.Error($0) }
+			.flatMap { json -> AnyPublisher<T, GraphQL.Error> in
+				let data = json["data"]
+				let errors = json["errors"]
+				if data.exists(),
+				   let result = T(data[operation.parsingKey])
+				{
+					return Just(result)
+						.setFailureType(to: GraphQL.Error.self)
+						.eraseToAnyPublisher()
+				} else if errors.exists(),
+				          let error = errors.arrayValue.compactMap(GraphQL.Error.init).first
+				{
+					return Fail(error: error)
+						.eraseToAnyPublisher()
+				} else {
+					return Fail(error: GraphQL.Error.somethingWentWrong)
+						.eraseToAnyPublisher()
+				}
+			}
+			.eraseToAnyPublisher()
+	}
+
+	// MARK: Helpers
+
+	private func httpRequest(for operation: GraphQL.Operation) -> URLRequest {
+		var urlRequest = URLRequest(url: URL(string: baseURL())!)
+		urlRequest.httpMethod = "POST"
+		urlRequest.httpBody = try? operation.request.json.rawData()
+		urlRequest.addValue("application/json", forHTTPHeaderField: "content-type")
+		if let token = accessToken() {
+			urlRequest.addValue(token, forHTTPHeaderField: "Authorization")
+		}
+		return urlRequest
+	}
+
+	private func logRequest(for operation: GraphQL.Operation) {
+		logger.log(
+			"GraphQL Request",
+			[
+				"Operation: \(operation)",
+				"Parameters: \(operation.request.variables)"
+			]
+		)
+	}
+
+	private func logResponse(
+		for operation: GraphQL.Operation,
+		output: URLSession.DataTaskPublisher.Output
+	) {
+		logger.log(
+			"GraphQL Response",
+			[
+				"Status: \((output.response as? HTTPURLResponse)?.statusCode ?? 0)",
+				"Operation: \(operation)",
+				"Parameters: \(operation.request.variables)",
+				"Data: \((try? JSON(data: output.data)) ?? [:])"
+			]
+		)
+	}
 }
 
 // MARK: - GraphQL
 
 enum GraphQL {
-    
-    struct Request {
-        var operationName: String
-        var operation: String
-        var variables: JSON = [:]
-        var json: JSON { JSON(dictionaryValue) }
-        
-        init(
-            operationName: String,
-            operation: String,
-            variables: JSON = [:]
-        ) {
-            self.operationName = operationName
-            self.operation = operation
-            self.variables = variables
-        }
-        
-        var dictionaryValue: [String: Any] {
-            var dictionary = [String: Any]()
-            dictionary["operationName"] = operationName
-            dictionary["query"] = operation
-            dictionary["variables"] = variables
-            return dictionary
-        }
-    }
-    
-    struct Error: Swift.Error {
-        var message: String
-        
-        init(_ message: String) {
-            self.message = message
-        }
-        
-        init(_ error: Swift.Error) {
-            message = error.localizedDescription
-        }
-        
-        init?(_ json: JSON) {
-            guard json.exists() else { return nil }
-            message = json["message"].stringValue
-        }
-        
-        static var somethingWentWrong: Self = .init("Something went wrong")
-    }
-    
-    enum Operation {
-        case launchesPast(limit: Int)
-        
-        var request: Request {
-            switch self {
-            case let .launchesPast(limit):
-                return .init(
-                    operationName: "LaunchesPast",
-                    operation: Query.launchesPast,
-                    variables: ["limit": limit]
-                )
-            }
-        }
-        
-        var parsingKey: String {
-            switch self {
-            case .launchesPast: return "launchesPast"
-            }
-        }
-    }
-    
-    enum Query {
-        static let launchesPast: String = """
-            query LaunchesPast($limit: Int!) {
-              launchesPast(limit: $limit) {
-                mission_name
-                launch_date_local
-              }
-            }
-        """
-    }
-    
+
+	struct Request {
+		var operationName: String
+		var operation: String
+		var variables: JSON = [:]
+		var json: JSON { JSON(dictionaryValue) }
+
+		init(
+			operationName: String,
+			operation: String,
+			variables: JSON = [:]
+		) {
+			self.operationName = operationName
+			self.operation = operation
+			self.variables = variables
+		}
+
+		var dictionaryValue: [String: Any] {
+			var dictionary = [String: Any]()
+			dictionary["operationName"] = operationName
+			dictionary["query"] = operation
+			dictionary["variables"] = variables
+			return dictionary
+		}
+	}
+
+	struct Error: Swift.Error {
+		var message: String
+
+		init(_ message: String) {
+			self.message = message
+		}
+
+		init(_ error: Swift.Error) {
+			message = error.localizedDescription
+		}
+
+		init?(_ json: JSON) {
+			guard json.exists() else { return nil }
+			message = json["message"].stringValue
+		}
+
+		static var somethingWentWrong: Self = .init("Something went wrong")
+	}
+
+	enum Operation {
+		case launchesPast(limit: Int)
+
+		var request: Request {
+			switch self {
+			case let .launchesPast(limit):
+				return .init(
+					operationName: "LaunchesPast",
+					operation: Query.launchesPast,
+					variables: ["limit": limit]
+				)
+			}
+		}
+
+		var parsingKey: String {
+			switch self {
+			case .launchesPast: return "launchesPast"
+			}
+		}
+	}
+
+	enum Query {
+		static let launchesPast: String = """
+		    query LaunchesPast($limit: Int!) {
+		      launchesPast(limit: $limit) {
+		        mission_name
+		        launch_date_local
+		      }
+		    }
+		"""
+	}
+
 }
 
 // MARK: - Logger
 
 struct Logger {
 
-    func log(_ info: String) {
-        print(info)
-    }
+	func log(_ info: String) {
+		print(info)
+	}
 
-    func log(_ title: String, _ info: [String]) {
-        log("\n----- \(title) -----\n\(info.joined(separator: "\n"))\n")
-    }
+	func log(_ title: String, _ info: [String]) {
+		log("\n----- \(title) -----\n\(info.joined(separator: "\n"))\n")
+	}
 
-    func log(_ title: String, _ info: String) {
-        log("\n----- \(title) -----\n\([info])\n")
-    }
+	func log(_ title: String, _ info: String) {
+		log("\n----- \(title) -----\n\([info])\n")
+	}
 
 }
 
 // MARK: - JSONParsable
 
 protocol JSONParsable {
-    init?(_ json: JSON)
+	init?(_ json: JSON)
 }
 
+// MARK: - Array + JSONParsable
+
 extension Array: JSONParsable where Element: JSONParsable {
-    init?(_ json: JSON) {
-        self = json.arrayValue.compactMap(Element.init)
-    }
+	init?(_ json: JSON) {
+		self = json.arrayValue.compactMap(Element.init)
+	}
 }
 
 // MARK: - Launch
 
 struct Launch: JSONParsable {
-    let missionName: String
-    let launchDateLocal: String
-    
-    init?(_ json: JSON) {
-        guard json.exists() else { return nil }
-        missionName = json["mission_name"].stringValue
-        launchDateLocal = json["launch_date_local"].stringValue
-    }
+	let missionName: String
+	let launchDateLocal: String
+
+	init?(_ json: JSON) {
+		guard json.exists() else { return nil }
+		missionName = json["mission_name"].stringValue
+		launchDateLocal = json["launch_date_local"].stringValue
+	}
 }
 
 // MARK: - Main
 
 let apiClient = ApiClient(
-    baseURL: { "https://api.spacex.land/graphql/" },
-    accessToken: { nil }
+	baseURL: { "https://api.spacex.land/graphql/" },
+	accessToken: { nil }
 )
 
 let query = apiClient
-    .perform(.launchesPast(limit: 10), as: [Launch].self)
-    .sink(
-        receiveCompletion: { print($0) },
-        receiveValue: { result in
-            print("\n--- OUTPUT ---")
-            print(result.map(\.missionName))
-        }
-    )
+	.perform(.launchesPast(limit: 10), as: [Launch].self)
+	.sink(
+		receiveCompletion: { print($0) },
+		receiveValue: { result in
+			print("\n--- OUTPUT ---")
+			print(result.map(\.missionName))
+		}
+	)
---
+++ UserDefaultsDatabase.playground/Contents.swift
@@ -0,0 +1,389 @@
+import Combine
+import Foundation
+import SwiftyJSON
+
+// MARK: - JSONEncodable
+
+protocol JSONEncodable: Equatable {
+	init?(_ json: JSON)
+}
+
+// MARK: - Serializable
+
+protocol Serializable {
+	func serialize() -> Data?
+	func toDict() -> [String: Any]
+}
+
+extension Serializable where Self: Encodable {
+	func serialize() -> Data? {
+		let encoder = JSONEncoder()
+		return try? encoder.encode(self)
+	}
+
+	func toDict() -> [String: Any] {
+		do {
+			guard let serialized = serialize() else { return [:] }
+			return try JSONSerialization.jsonObject(with: serialized, options: []) as? [String: Any] ?? [:]
+		} catch {
+			print("Can't convert to dictionary")
+			return [:]
+		}
+	}
+}
+
+// MARK: - LocalDatabaseObject
+
+protocol LocalDatabaseObject: Identifiable & Serializable & JSONEncodable {}
+
+// MARK: - LocalDatabaseMappable
+
+protocol LocalDatabaseMappable {
+	associatedtype DatabaseObject: LocalDatabaseObject
+	var databaseObject: DatabaseObject { get }
+	static func map(_ databaseObject: DatabaseObject) -> Self
+}
+
+// MARK: - DatabaseNotifierProtocol
+
+protocol DatabaseNotifierProtocol {
+	typealias Notification<T: Identifiable> = DatabaseNotifier.Notification<T>
+
+	func post<T: LocalDatabaseObject>(
+		_ type: T.Type,
+		_ notification: Notification<T>
+	)
+
+	func register<T: LocalDatabaseObject, U: LocalDatabaseMappable>(
+		_ type: U.Type,
+		map: @escaping (_ databaseObject: T) -> U
+	) -> AnyPublisher<Notification<U>, Never>
+}
+
+extension DatabaseNotifierProtocol {
+
+	func register<T: LocalDatabaseMappable>(
+		_ type: T.Type
+	) -> AnyPublisher<Notification<T>, Never> {
+		register(T.self, map: T.map).eraseToAnyPublisher()
+	}
+
+}
+
+// MARK: - LocalDatabaseObservationToken
+
+protocol LocalDatabaseObservationToken {
+	func invalidate()
+}
+
+// MARK: - Notification.Name + Extensions
+
+extension Notification.Name {
+	static let localDatabaseObjectUpdate = Notification.Name("local_database_object_update")
+}
+
+// MARK: - DatabaseNotifier
+
+final class DatabaseNotifier: DatabaseNotifierProtocol {
+
+	// MARK: Properties
+
+	private lazy var notificationCenter = NotificationCenter.default
+
+	// MARK: Observation
+
+	func post<T: LocalDatabaseObject>(
+		_ type: T.Type,
+		_ notification: Notification<T>
+	) {
+		notificationCenter.post(
+			name: .localDatabaseObjectUpdate,
+			object: notification,
+			userInfo: nil
+		)
+	}
+
+	func register<T: LocalDatabaseObject, U: LocalDatabaseMappable>(
+		_ type: U.Type,
+		map: @escaping (_ databaseObject: T) -> U
+	) -> AnyPublisher<Notification<U>, Never> {
+		NotificationCenter.Publisher(center: .default, name: .localDatabaseObjectUpdate, object: nil)
+			.compactMap { $0.object as? Notification<T> }
+			.compactMap { notification -> Notification<U>? in
+				switch notification {
+				case let .delete(id):
+					if let id = id as? U.ID { return .delete(id) }
+					else { return nil }
+				case let .update(old, new):
+					return .update(map(old), map(new))
+				case let .add(new):
+					return .add(map(new))
+				}
+			}
+			.eraseToAnyPublisher()
+	}
+
+}
+
+extension DatabaseNotifier {
+
+	enum Notification<T: Identifiable> {
+		case delete(_ id: T.ID)
+		case update(_ old: T, _ new: T)
+		case add(_ data: T)
+	}
+
+}
+
+// MARK: - Item
+
+struct Item: Identifiable, JSONEncodable, Codable {
+
+	typealias ID = String
+
+	// MARK: Properties
+
+	var id: ID = ""
+	var title: String = ""
+
+	// MARK: Life Cycle
+
+	init(id: ID, title: String) {
+		self.id = id
+		self.title = title
+	}
+
+	init?(_ json: JSON) {
+		guard json["id"].string != nil else { return nil }
+		id = json["id"].stringValue
+		title = json["title"].stringValue
+	}
+
+	init(_ item: ItemDatabaseObject) {
+		id = item.id
+		title = item.title
+	}
+
+}
+
+// MARK: - ItemDatabaseObject
+
+struct ItemDatabaseObject: LocalDatabaseObject, Codable {
+
+	// MARK: Properties
+
+	var id: String = ""
+	var title: String = ""
+
+	// MARK: Life Cycle
+
+	init?(_ json: JSON) {
+		guard json["id"].string != nil else { return nil }
+		id = json["id"].stringValue
+		title = json["title"].stringValue
+	}
+
+	init(_ item: Item) {
+		id = item.id
+		title = item.title
+	}
+
+}
+
+// MARK: - Item + LocalDatabaseMappable
+
+extension Item: LocalDatabaseMappable {
+
+	var databaseObject: ItemDatabaseObject {
+		.init(self)
+	}
+
+	static func map(_ databaseObject: ItemDatabaseObject) -> Item {
+		.init(databaseObject)
+	}
+
+}
+
+// MARK: - LocalDatabase
+
+protocol LocalDatabase {
+	var notifier: DatabaseNotifierProtocol { get }
+
+	func get<T: LocalDatabaseObject>(_ type: T.Type) -> [T]
+	func get<T: LocalDatabaseObject, Key>(_ type: T.Type, primaryKey: Key) -> T?
+
+	func add<T: LocalDatabaseObject>(_ objects: [T])
+	func add<T: LocalDatabaseObject>(_ object: T)
+	func write(_ operations: () -> Void)
+
+	func delete<T: LocalDatabaseObject>(_ object: T)
+	func delete<T: LocalDatabaseObject>(objects: [T])
+	func flush()
+
+	func log<T: LocalDatabaseObject>(type: T.Type)
+}
+
+// MARK: - UserDefaultsLocalDatabase
+
+final class UserDefaultsLocalDatabase: LocalDatabase {
+
+	// MARK: Properties
+
+	let userDefaults = UserDefaults.standard
+	let notifier: DatabaseNotifierProtocol = DatabaseNotifier()
+
+	static let databaseKey = "local_database"
+	static let normalizedKey = "local_database_normalized"
+
+	private var databaseKey = UserDefaultsLocalDatabase.databaseKey
+	private var normalizedKey = UserDefaultsLocalDatabase.normalizedKey
+
+	private var storableEntityTypes: [String] {
+		[
+			String(describing: Item.self)
+		]
+	}
+
+	// MARK: Read
+
+	func get<T: LocalDatabaseObject>(_ type: T.Type = T.self) -> [T] {
+		let key = databaseKey + "_" + String(describing: type)
+		if let table = userDefaults.dictionary(forKey: key) as? [T.ID: [String: Any]] {
+			return table.values.compactMap { T(JSON($0)) }
+		} else {
+			return []
+		}
+	}
+
+	func get<T: LocalDatabaseObject, Key>(
+		_ type: T.Type = T.self,
+		primaryKey: Key
+	) -> T? {
+		let key = databaseKey + "_" + String(describing: type)
+		if let table = userDefaults.dictionary(forKey: key) as? [T.ID: [String: Any]],
+		   let primaryKey = primaryKey as? T.ID,
+		   let data = table[primaryKey]
+		{
+			return T(JSON(data))
+		} else {
+			return nil
+		}
+	}
+
+	// MARK: Write
+
+	func add<T: LocalDatabaseObject>(_ objects: [T]) {
+		let typeKey = String(describing: T.self)
+		let key = databaseKey + "_" + typeKey
+		var table = userDefaults.dictionary(forKey: key) as? [T.ID: [String: Any]] ?? [:]
+		var oldMap: [T.ID: T] = [:]
+		objects.forEach {
+			oldMap[$0.id] = T(JSON(table[$0.id] ?? [:]))
+			table[$0.id] = $0.toDict()
+			userDefaults.setValue($0.toDict(), forKey: "\(normalizedKey).\(typeKey).\($0.id)")
+			if let old = oldMap[$0.id] { notifier.post(T.self, .update(old, $0)) }
+			else { notifier.post(T.self, .add($0)) }
+		}
+		userDefaults.setValue(table, forKey: key)
+	}
+
+	func add<T: LocalDatabaseObject>(_ object: T) {
+		let typeKey = String(describing: T.self)
+		let key = databaseKey + "_" + typeKey
+		var table = userDefaults.dictionary(forKey: key) as? [T.ID: [String: Any]] ?? [:]
+		let old = T(JSON(table[object.id] ?? [:]))
+		table[object.id] = object.toDict()
+		userDefaults.setValue(object.toDict(), forKey: "\(normalizedKey).\(typeKey).\(object.id)")
+		userDefaults.setValue(table, forKey: key)
+
+		if let old = old { notifier.post(T.self, .update(old, object)) }
+		else { notifier.post(T.self, .add(object)) }
+	}
+
+	func write(_ operations: () -> Void) {
+		operations()
+	}
+
+	func delete<T: LocalDatabaseObject>(_ object: T) {
+		let typeKey = String(describing: T.self)
+		let key = databaseKey + "_" + typeKey
+		var table = userDefaults.dictionary(forKey: key) as? [T.ID: [String: Any]] ?? [:]
+
+		table[object.id] = nil
+		userDefaults.removeObject(forKey: "\(normalizedKey).\(typeKey).\(object.id)")
+		userDefaults.setValue(table, forKey: key)
+		notifier.post(T.self, .delete(object.id))
+	}
+
+	func delete<T: LocalDatabaseObject>(objects: [T]) {
+		let typeKey = String(describing: T.self)
+		let key = databaseKey + "_" + typeKey
+		var table = userDefaults.dictionary(forKey: key) as? [T.ID: [String: Any]] ?? [:]
+
+		objects.forEach {
+			table[$0.id] = nil
+			userDefaults.removeObject(forKey: "\(normalizedKey).\(typeKey).\($0.id)")
+		}
+		userDefaults.setValue(table, forKey: key)
+		objects.forEach { notifier.post(T.self, .delete($0.id)) }
+	}
+
+	func flush() {
+		UserDefaults.standard.dictionaryRepresentation().keys
+			.filter { $0.contains(databaseKey) }
+			.forEach { userDefaults.setValue([:], forKey: $0) }
+	}
+
+	// MARK: Log
+
+	func log<T: LocalDatabaseObject>(type: T.Type) {
+		let typeKey = String(describing: T.self)
+		let key = databaseKey + "_" + typeKey
+		let table = userDefaults.dictionary(forKey: key) as? [T.ID: [String: Any]] ?? [:]
+		let tableJSON = JSON(table)
+
+		if let data = try? tableJSON.rawData(),
+		   let directory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+		{
+			let databasePath = directory.appendingPathComponent("user_defaults_\(typeKey).json")
+			do { try data.write(to: databasePath) }
+			catch { print(error) }
+			print(databasePath)
+		}
+
+		print(JSON(table))
+	}
+}
+
+// MARK: - Main
+
+var bag: Set<AnyCancellable> = []
+let localDatabase: LocalDatabase = UserDefaultsLocalDatabase()
+let notifier: DatabaseNotifierProtocol = localDatabase.notifier
+
+localDatabase.flush()
+notifier.register(Item.self)
+	.receive(on: DispatchQueue.main)
+	.sink { item in print(item) }
+	.store(in: &bag)
+
+let item1 = Item(id: "1", title: "item1")
+let item2 = Item(id: "2", title: "item2")
+localDatabase.add(item1.databaseObject)
+localDatabase.add(item2.databaseObject)
+
+DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+	let _item2 = Item(id: "2", title: "item2_updated")
+	localDatabase.add(_item2.databaseObject)
+}
+
+DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+	let _item1 = Item(id: "1", title: "item1_updated")
+	localDatabase.add(_item1.databaseObject)
+}
+
+DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+	if let databaseObject = localDatabase.get(ItemDatabaseObject.self, primaryKey: "1") {
+		localDatabase.delete(databaseObject)
+	}
+}
